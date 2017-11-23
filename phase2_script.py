00abf6422f4ec753de0478a8b28db013410d9b74
Siddhant Khanna <skhanna1@cs.ualberta.ca>
2017-11-23 00:02:22 -0700

    Phas 2 I believe done

--- phase2_script.py
+++ phase2_script.py
@@ -1,54 +1,91 @@
 # CMPUT 291 - MiniProject 2 - Code for Phase 2
 import os
+import subprocess
 
 def main():
-    # Sort .txt files
-    try:
-        createSortedfile('terms.txt')
-        createSortedfile('years.txt')
-        createSortedfile('recs.txt')
-    except:
-        print("Sorting Failed. Files do not exist!")
-
-    # (1) a hash index on recs.txt with record key as keys and the full record as data.
-    CreateRecsIndex()
-    # (2) a B+-tree index on terms.txt with terms as keys and record key as data.
-    CreateTermsIndex()
-    # (3) a B+-tree index on years.txt with years as keys and record key as data.
-    CreateYearsIndex()
+	# Sort .txt files
+	try:
+		createSortedfile('terms.txt')
+		createSortedfile('years.txt')
+		createSortedfile('recs.txt')
+	except:
+		print("Sorting Failed. Files do not exist!")
 
 
-def CreateRecsIndex():
-    # Code to create Hash index for recs.txt
+	try:
+		# (1) a hash index on recs.txt with record key as keys and the full record as data.
+		CreateRecsIndex('s_recs.txt')
+		# (2) a B+-tree index on terms.txt with terms as keys and record key as data.
+		CreateTermsIndex('s_terms.txt')
+		# (3) a B+-tree index on years.txt with years as keys and record key as data.
+		CreateYearsIndex('s_years.txt')
+	except:
+		print("Could not create index files.")
+	return
 
-    return
+def CreateRecsIndex(sorted_filename):
+	try:
+		# Code to create Hash index for recs.txt
+		#first create new files for removing Backslash
+		noBackSlashFile = "noBack" + sorted_filename
+		command = "perl break.pl" + " <" + sorted_filename + "> " + noBackSlashFile
+		print(command)
+		os.system(command)
+
+		commandForIdx = "awk -F: '{print $1; print $0}' < " + noBackSlashFile + " | db_load -T -t hash re.idx"
+		print(commandForIdx)
+		os.system(commandForIdx)
+	except:
+		print("Could not create File with no Backslash or Index file could not be created.")
+
+	return
 
 
-def CreateTermsIndex():
-    # Code to create B+-Tree index for terms.txt
+def CreateTermsIndex(sorted_filename):
+	try:
+		# Code to create B+-Tree index for terms.txt
+		noBackSlashFile = "noBack" + sorted_filename
+		command = "perl break.pl" + " <" + sorted_filename + "> " + noBackSlashFile
+		print(command)
+		os.system(command)
 
-    return
+		commandForIdx = "awk -F: '{print $1; print $0}' < " + noBackSlashFile + " | db_load -T -t btree te.idx"
+		print(commandForIdx)
+		os.system(commandForIdx)
+	except:
+		print("Could not create File with no Backslash or Index file could not be created.")
+	return
 
-def CreateYearsIndex():
-    # Code to create B+-Tree index for years.txt
+def CreateYearsIndex(sorted_filename):
+	try:
+		# Code to create B+-Tree index for years.txt
+		noBackSlashFile = "noBack" + sorted_filename
+		command = "perl break.pl" + " <" + sorted_filename + "> " + noBackSlashFile
+		print(command)
+		os.system(command)
 
-    return
+		commandForIdx = "awk -F: '{print $1; print $0}' < " + noBackSlashFile + " | db_load -T -t btree ye.idx"
+		print(commandForIdx)
+		os.system(commandForIdx)
+	except:
+		print("Could not create File with no Backslash or Index file could not be created.")
+	return
 
 
 
 
 def createSortedfile(filename):
-# Function calls linux sort on the .txt files.
-# For now the sorted lines are written to s_'filename'. May need to change later
-    try:
-        o_name = "s_"+filename
-        out = open(o_name, "w")
-        out.close()
-        command = "sort -u -o "+o_name+" "+filename
-        print(command)
-        os.system(command)
-    except:
-        print("Sorting Failed.")
+	# Function calls linux sort on the .txt files.
+	# For now the sorted lines are written to s_'filename'. May need to change later
+	try:
+		o_name = "s_"+filename
+		out = open(o_name, "w")
+		out.close()
+		command = "sort -u -o "+o_name+" "+filename
+		print(command)
+		os.system(command)
+	except:
+		print("Sorting Failed.")
 
 if __name__ == '__main__':
-    main()
+	main()
